c2cab6867a6c98904adc3eeea3d9389cec14982b
sabuwu <sabuwu091@gmail.com>
2026-06-14 02:02:00 +0000

    feat: rotas de catalogo, promocoes, biblioteca, downloads adicionadas

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,28 @@ use App\Http\Controllers\LandingController;
 use App\Http\Controllers\DashboardController;
 
 
-Route::get('/', function () {
-    return view("landing");
-});
+Route::get('/', function () { return view("landing"); });
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/catalogo', function () { 
+    return view('pages.layout.catalogo'); 
+})->middleware(['auth', 'verified'])->name('catalogo');
+
+Route::get('/promocoes', function () {
+    return view('pages.layout.promocoes');
+})->middleware(['auth', 'verified'])->name('promocoes');
+
+Route::get('/biblioteca', function () { 
+    return view('pages.layout.biblioteca'); 
+})->middleware(['auth', 'verified'])->name('biblioteca');
+
+Route::get('/downloads', function () { 
+    return view('pages.layout.downloads'); 
+})->middleware(['auth', 'verified'])->name('downloads');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
